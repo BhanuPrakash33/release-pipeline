765762b79e107a17e11edda40759b7cc904da5ed
root <root@ip-172-31-34-167.ap-south-1.compute.internal>
2021-06-19 10:00:05 +0000

    added files

---
+++ remote.tf
@@ -0,0 +1,19 @@
+resource "null_resource" "allow-ufw" {
+  
+    connection {
+    type = "ssh"
+    host = aws_instance.kubeadm.public_ip
+    user = "ubuntu"
+    private_key = file("kubead-key")
+    }
+
+provisioner "remote-exec" {
+    inline = [
+      "sudo ufw allow 22/tcp",
+      
+    ]
+  }
+
+  depends_on = [ aws_instance.kubeadm ]
+
+  }
